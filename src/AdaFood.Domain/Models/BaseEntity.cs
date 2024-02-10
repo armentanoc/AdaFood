14c89e655ebb80c184cb1f64f39e3822c2cae010
Carolina Armentano <armentanocarolina@gmail.com>
2024-02-10 10:19:44 -0300

    feat: domain layer.

---
+++ src/AdaFood.Domain/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿
+namespace AdaFood.Domain.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/AdaFood.Domain/Models/DeliveryDriver.cs
@@ -0,0 +1,14 @@
+﻿namespace AdaFood.Domain.Models
+{
+    public class DeliveryDriver : BaseEntity
+    {
+        public string Name { get; set; }
+        public string CPF { get; set; }
+
+        public DeliveryDriver(string name, string cpf) 
+        {
+            Name = name;
+            CPF = cpf;
+        }
+    }
+}
